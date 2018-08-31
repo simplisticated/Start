2e52b0efb648c08ed084ac587525b137acf22e15
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-08-31 22:28:49 +0300

    Added LaunchCounter class

---
+++ Source/LaunchCounter.swift
@@ -0,0 +1,56 @@
+//
+//  LaunchCounter.swift
+//  Start
+//
+//  Created by Igor Matyushkin on 31.08.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public final class LaunchCounter {
+    
+    // MARK: Class variables & properties
+    
+    internal static let shared = {
+        return LaunchCounter()
+    }()
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    fileprivate init() {
+    }
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Object variables & properties
+    
+    public var count: Int {
+        get {
+            return DefaultsManager.shared.launchCount
+        }
+    }
+    
+    // MARK: Public object methods
+    
+    public func increment() {
+        let defaultsManager = DefaultsManager.shared
+        let currentLaunchCount = defaultsManager.launchCount
+        let newLaunchCount = currentLaunchCount + 1
+        defaultsManager.launchCount = newLaunchCount
+    }
+    
+    public func reset() {
+        DefaultsManager.shared.launchCount = 0
+    }
+    
+    // MARK: Private object methods
+    
+}
